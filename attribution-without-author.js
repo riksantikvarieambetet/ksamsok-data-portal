90413244782209c079ea7d8b35fb79bb1e284524
abbe98 <albin.post@gmail.com>
2020-06-01 16:29:36 +0200

    attribution without author check

---
+++ attribution-without-author.js
@@ -0,0 +1,36 @@
+document.querySelector('#runBtn').addEventListener('click', e => {
+  e.preventDefault();
+  const url = endpoint + 'ksamsok/api?method=search&hitsPerPage=500&fields=url&query=(byline=*okänd* OR byline=*saknas*) AND mediaLicense=*by* AND serviceOrganization=' + select.value + '&recordSchema=xml';
+
+  fetch(url, {
+    headers: {
+      'Accept': 'application/json',
+    }
+  }).then(response => {
+    return response.json();
+  }).then(data => {
+    if (!data.result.totalHits) {
+      renderMessage('Inga möjliga fel kunde upptäckas.');
+      return;
+    }
+
+    var tableData = [['Källa']];
+    data.result.records.record.forEach(record => {
+      record = parseFieldsRecord(record);
+      var source = document.createElement('a');
+      source.href = record['url'];
+      source.append(document.createTextNode('Länk till källsystem'));
+
+      tableData.push([source]);
+    });
+
+    renderTable(tableData);
+  }).catch(e => {
+    console.log(e, 'booo');
+  });
+});
+
+
+// **** INIT ****
+
+populateOrgsSelect();
